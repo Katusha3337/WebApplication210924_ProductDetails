9b22d51f6be8ef4862b8e60466c685f91879efbe
Marka_rb95@student.itstep.org <Marka_rb95@student.itstep.org>
2024-09-29 21:56:54 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication210924_ProductDetails.Models;
+
+namespace WebApplication210924_ProductDetails.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            var product = new Product
+            {
+                Id = id,
+                Name = "Пример продукта",
+                Description = "Описание продукта",
+                Price = 99.99M
+            };
+
+            return View("ProductDetails", product);
+        }
+
+        [HttpPost]
+        public IActionResult AddReview(string review)
+        {
+            return RedirectToAction("Details", new { id = 1 });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication210924_ProductDetails.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
